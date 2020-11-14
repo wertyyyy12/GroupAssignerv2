869df04b7fe7429f689d98d07671c23d5970b560
wertyyyy12 <43356692+wertyyyy12@users.noreply.github.com>
2020-11-13 22:51:17 -0800

    (v0.2.6) Cost calculations for swap alg complete

--- js/alg.js
+++ js/alg.js
@@ -1,18 +1,52 @@
-// const { group } = require("console");
-// const e = require("express");
-// const { remove } = require("fs-extra");
-// const { array } = require("yargs");
+var testList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var testPrefs = [
+  [1, [3, 6]],
+  [2, [3, 9]],
+  [3, [5]],
+  [4, [3]],
+  [5, [2]],
+  [6, [2, 1]],
+  [7, [4, 6, 5]],
+  [8, [4, 6]],
+  [9, [5]]
+];
 
-const { group } = require("console");
-const { completion } = require("yargs");
+function splitIntoGroups(groupSize, studentList) {
+  var numGroups = Math.ceil(studentList.length / groupSize);
+  var splitGroups = [];
+  for (var i = 0; i < numGroups; i++) {
+    splitGroups.push([]);
+  }
 
-function arrayRemoveSet(array, removeArray) {
-  var arrayCopy = array;
-  removeArray.forEach((element) => {
-    arrayCopy = arrayCopy.filter(elem => elem != element);
+  var j = 0;
+  for (var i = 0; i < studentList.length; i++) {
+    if (splitGroups[j].length >= groupSize) {
+      j++;
+    }
+    splitGroups[j].push(studentList[i]);
+  }
+
+  return splitGroups;
+}
+
+function arrayRemove(array, element) {
+  var arrayCopy = JSON.parse(JSON.stringify(array));
+  return arrayCopy.filter(elem => elem != element);
+}
+
+function findWanting(person, prefs) { //find who wants a certain person in their group
+  var people = [];
+  prefs.forEach((pref) => {
+    var student = pref[0];
+    var studentPrefs = pref[1];
+
+    if (studentPrefs.includes(person)) {
+      people.push(student);    
+    }
   });
 
-  return arrayCopy;
+  return people;
+
 }
 
 function findStudentPrefs(student, prefs) {
@@ -26,157 +60,63 @@ function findStudentPrefs(student, prefs) {
   return studentPrefs;
 }
 
-function generateCombinations(sourceArray, comboLength) {
-  const sourceLength = sourceArray.length;
-  if (comboLength > sourceLength) return [];
+//A swaps with B
 
-  const combos = []; // Stores valid combinations as they are generated.
+function findOptimum(sG, prefs) {
+  var splitGroups = JSON.parse(JSON.stringify(sG));
+  prefs.forEach((pref) => {
+    var student = pref[0];
+    var studentPrefs = pref[1];
+    var studentGroup = splitGroups.find(group => group.includes(student));
 
-  const makeNextCombos = (workingCombo, currentIndex, remainingCount) => {
-    const oneAwayFromComboLength = remainingCount == 1;
-
-    for (let sourceIndex = currentIndex; sourceIndex < sourceLength; sourceIndex++) {
-      const next = [ ...workingCombo, sourceArray[sourceIndex] ];
-
-      if (oneAwayFromComboLength) {
-        combos.push(next);
-      }
-      else {
-        makeNextCombos(next, sourceIndex + 1, remainingCount - 1);
-      }
-        }
-  }
-
-  makeNextCombos([], 0, comboLength);
-  return combos;
-}
-
-function endSwap(array, newArray) {
-	var copy = JSON.parse(JSON.stringify(array)); 
-	Array.prototype.splice.apply(copy, [copy.length-newArray.length, newArray.length].concat(newArray));
-  return copy;
-}
-
-
-// console.log(endSwap([1, 2, 3, 4], [4234, 23234]));
-
-
-var testPrefs = [
-  [1, [7, 3]], 
-  [2, [7, 1]], 
-  [3, [1]], 
-  [4, [5, 2]], 
-  [5, [3, 7]], 
-  [6, [1]], 
-  [7, [1]],
-  [8, [1]],
-  [9, [1]],
-  [10, [1]],
-  [11, [1]],
-  [12, [1]],
-  [13, [1]],
-  [14, [1]],
-  [15, [1]],
-  [16, [1]],
-  [17, [1]]
-];
-var testList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
-var sampleGroupArray = [];
-
-
-function possibleGroups(list, groupSize, prefs) {
-  var numGroups = Math.ceil(list.length / groupSize);
-  // console.log(pickSlot(list, groupSize, numGroups).length);
-  var final;
-  function evaluate(groupSet, prefs) {
-    var cost = 0;
-    prefs.forEach((pref) => {
-      var studentGroup = groupSet.find(group => group.includes(pref[0]));
-      if (studentGroup) {
-        pref[1].forEach((person) => {
-          if (!studentGroup.includes(person)) {
-            cost++;
-          };
+    studentPrefs.forEach((person) => {
+      var personGroup = splitGroups.find(group => group.includes(person));
+      var gain = 0;
+      if (personGroup != studentGroup) { //the person wants a swap; "student" wants to swap with "person" (student = A, person = B)
+        //COST calculations (anything that violates prefrences)
+        var swapPeople = arrayRemove(personGroup, person);
+        var wantingPersonB = findWanting(person, prefs);
+        var wantingPersonA = findWanting(student, prefs);
+        swapPeople.forEach((swapPerson) => { //for every person to swap with
+          if (wantingPersonB.includes(swapPerson)) { //then a swap person (a person in pB's group) wanted B
+            gain--;
+          }
         });
 
-      }
-      // console.log(cost);
-    });
-    // console.log(cost);
-
-    return cost;
-
-  }
-
-  var minCost = 99999999999999999999;
-  function pickSlot(list, groupSize, nG, prefs) { //nG = num groups
-    var prefs = testPrefs;
-    generateCombinations(list, groupSize).forEach((set) => {
-
-      sampleGroupArray.push(set);
-      var newSource = arrayRemoveSet(list, set);
-      if (newSource.length > 0) {
-        if (newSource.length >= groupSize) {
-          pickSlot(newSource, groupSize, nG);
-        }
-        else {
-          sampleGroupArray.push(newSource);
-          var currentCost = evaluate(sampleGroupArray, prefs);
-          if (currentCost < minCost) {
-            console.log(currentCost);
-            minCost = currentCost;
-            final = sampleGroupArray;
+        //check if person B wanted anyone in their group
+        var personBprefs = findStudentPrefs(person, prefs);
+        personGroup.forEach((BgroupMember) => {
+          if (personBprefs[1].includes(BgroupMember)) {
+            gain--;
           }
-          // final.push(sampleGroupArray);
-          sampleGroupArray = [];
-        }
-      }
+        });
 
-
-      if (newSource.length == 0) {
-        if (sampleGroupArray.length >= nG) {
-          // final[index] = sampleGroupArray;
-          // final.push(sampleGroupArray);
-          var currentCost = evaluate(sampleGroupArray, prefs);
-
-          if (currentCost < minCost) {
-            console.log(currentCost);
-            minCost = currentCost;
-            final = sampleGroupArray;
+        //check if any person in group A wanted A
+        studentGroup.forEach((AgroupPerson) => { //for every person to swap with
+          if (wantingPersonA.includes(AgroupPerson)) { //then a swap person (a person in pB's group) wanted B
+            gain--;
           }
-          // console.log(sampleGroupArray);
-          sampleGroupArray = [];
-        }
+        });
 
-        else {
-          var lastOutput = final[final.length-1];
-          // var lOC = JSON.parse(JSON.stringify(lastOutput));
-          // console.log(lOC);
-          var newOutput = endSwap(lastOutput, sampleGroupArray);
-          sampleGroupArray = [];
-          var currentCost = evaluate(newOutput, prefs);
-
-          if (currentCost < minCost) {
-            console.log(currentCost);
-            minCost = currentCost;
-            final = sampleGroupArray;
+        var personAprefs = findStudentPrefs(student, prefs); //check if A wanted anyone in their group
+        studentGroup.forEach((AgroupMember) => {
+          if (personAprefs[1].includes(AgroupMember)) {
+            gain--;
           }
-          // final.push(newOutput);
+        });
+        
+        console.log(gain);
 
-        }
+        //GAIN calculations
+        
+
+
       }
     });
-    // console.log(final);
-    return final;
-  } 
-  return pickSlot(list, groupSize, numGroups, prefs);
+  });
 }
-// console.log(possibleGroups(0, testList, 3));
-// possibleGroups(testList, 4).forEach((group) => {
-//   console.log(group);
-// })
-// console.log(possibleGroups(testList, 4, testPrefs));
 
 
-
-// // findOptimum(testPrefs, testList, 4);
+var testSplitGroups = splitIntoGroups(3, testList);
+console.log(testSplitGroups);
+findOptimum(splitIntoGroups(3, testList), testPrefs);
